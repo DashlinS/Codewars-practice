0d764ea2976acee055b30338ec1061c600f44185
Dashlin Sermeil <dashlin.sermeil@gmail.com>
2021-05-31 17:34:37 -0400

    adding  codewar

---
+++ Ruby/multiply.rb
@@ -0,0 +1,8 @@
+=begin 
+https://www.codewars.com/kata/50654ddff44f800200000004/train/ruby
+This code does not execute properly. Try to figure out why.
+=end
+
+def multiply(a, b)
+  return a * b
+end
