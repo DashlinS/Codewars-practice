3ca9f8d296d954d22b4a6c123862903bd05ef049
Dashlin Sermeil <dashlin.sermeil@gmail.com>
2021-06-06 17:41:36 -0400

    adding  codewar

---
+++ Ruby/squareNSum.js
@@ -0,0 +1,12 @@
+/*
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+*/
+
+function squareSum(numbers) {
+  return numbers.map((x) => x * x).reduce((a, b) => a + b, 0);
+}
+
+/* Second solution when revisited*/
+let squareSum = (numbers) =>
+  numbers.map((number) => Math.pow(number, 2)).reduce((a, b) => a + b, 0);
